6eac8cdb9a672abcc5bfe0f87312ccd128870929
João Siles <jpsileskh@hotmail.com>
2025-02-24 09:30:47 -0300

    Aula java

---
+++ aula02/Variaveis.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class Variaveis {
+    public static void main(String[] args) {
+      String msg = "Olá tudo bom?";
+      System.out.println(msg);
+      int n1 = 3;
+      int n2 = 4;
+      System.out.println(n1);
+      System.out.println(n1 + n2);
+      JOptionPane.showMessageDialog(null, msg, msg, 0);      
+    }
+  }
+  
